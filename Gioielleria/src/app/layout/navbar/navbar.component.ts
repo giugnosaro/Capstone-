b5f919dc32ea886daad41867aec3b295191315a4
Giugno Rosario <164650117+giugnosaro@users.noreply.github.com>
2024-08-26 19:43:33 +0200

    Ts video

--- Gioielleria/src/app/layout/navbar/navbar.component.ts
+++ Gioielleria/src/app/layout/navbar/navbar.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  
+  
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const videoElement = document.getElementById("background-video") as HTMLVideoElement;
+
+  if (videoElement) {
+    videoElement.play().catch(error => {
+      console.error('Autoplay non riuscito:', error);
+      // Eventualmente, puoi gestire l'errore, ad esempio mostrando un pulsante per far partire il video manualmente
+    });
+  } else {
+    console.error('Elemento video non trovato.');
+  }
+});
+
