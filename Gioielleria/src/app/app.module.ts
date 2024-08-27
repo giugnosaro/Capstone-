56ac121faffff5d24907999d4b4531b6793cd8bc
Giugno Rosario <164650117+giugnosaro@users.noreply.github.com>
2024-08-27 11:37:04 +0200

    carosello gioielli e resi e spedizione

--- Gioielleria/src/app/app.module.ts
+++ Gioielleria/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { CarouselComponent } from './home/carousel/carousel.component';
+import { GaranzieComponent } from './home/garanzie/garanzie.component';
+import { CarouselgioielliComponent } from './home/carouselgioielli/carouselgioielli.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { CarouselComponent } from './home/carousel/carousel.component';
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    CarouselComponent
+    CarouselComponent,
+    GaranzieComponent,
+    CarouselgioielliComponent
   ],
   imports: [
     BrowserModule,
---
+++ Gioielleria/src/app/home/garanzie/garanzie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-garanzie',
+  templateUrl: './garanzie.component.html',
+  styleUrls: ['./garanzie.component.css']
+})
+export class GaranzieComponent {
+
+}
