10097be71560f59af1a522f51f767428ac3a1b08
Giugno Rosario <164650117+giugnosaro@users.noreply.github.com>
2024-08-30 16:23:50 +0200

    migliorie

--- Gioielleria/src/app/layout/navbar/navbar.component.ts
+++ Gioielleria/src/app/layout/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
-
 
 @Component({
   selector: 'app-navbar',
@@ -9,23 +8,34 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
   user: any = null;
+  isAuthenticated: boolean = false;
+  isProfileModalOpen: boolean = false;
 
-  constructor(private userService: UserService , private router: Router) {}
-  
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
     this.user = this.userService.getUser();
+    this.isAuthenticated = !!this.user; // Verifica se l'utente è autenticato
+  }
+
+  openProfileModal() {
+    this.isProfileModalOpen = true;
+  }
+
+  closeProfileModal() {
+    this.isProfileModalOpen = false;
   }
 
   logout() {
     this.userService.clearUser();
+    this.isAuthenticated = false;
+    this.closeProfileModal();
     this.router.navigate(['/login']);
   }
-
 }
 
+// Gestione del video di sfondo
 document.addEventListener("DOMContentLoaded", () => {
   const videoElement = document.getElementById("background-video") as HTMLVideoElement;
 
@@ -38,4 +48,3 @@ document.addEventListener("DOMContentLoaded", () => {
     console.error('Elemento video non trovato.');
   }
 });
-
